054c2bb496d7585d3d20794a4846b357fb403bb6
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2025-01-14 08:46:50 -0300

    feat: exercício cinco criado script

---
+++ script-5.js
@@ -0,0 +1,6 @@
+    // 5 Escreva um programa  que faça a inversão de uma string 
+
+
+    const frase = prompt("Escreva uma frase");
+    const novaFrase = frase.split("").reverse().join("")
+    alert(novaFrase)
